45c368bbf828a6272a66705375e48704993aca69
davidgavrilut <davidgavrilut1@gmail.com>
2024-01-05 09:51:56 +0200

    Entities, DbContext, Migrations

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+}
